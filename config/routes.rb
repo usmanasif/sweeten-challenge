79a6d368dccf5695413387608e9cc236f795b74a
Usman Asif <usmanasif@users.noreply.github.com>
2019-02-18 06:26:56 -0800

    Added Necessary Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :renovation_projects, only: [:index, :new, :create, :show]
   resources :general_contractors, only: [:index, :new, :create, :show]
+  resources :contracts, only: [:create, :update]
+
+  root 'home#index'
 end
